6c2a519eb9ff74935a9332b37e746fa410f1fb4f
ahlpritz <ztirp1@gmail.com>
2023-09-12 15:23:45 +0800

    added api for MenuItem to Get

---
+++ Pritz_API/Controllers/MenuItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pritz_API.Data;
+
+namespace Pritz_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            return Ok(_db.MenuItems);
+        }
+    }
+}
