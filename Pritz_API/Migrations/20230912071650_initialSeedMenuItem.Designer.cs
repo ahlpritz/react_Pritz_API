a997fd8e0ab72af0f13ea2983318d8416875d606
ahlpritz <ztirp1@gmail.com>
2023-09-12 15:18:17 +0800

    added seed menu

--- Pritz_API/Migrations/20230912071650_initialSeedMenuItem.Designer.cs
+++ Pritz_API/Migrations/20230912071650_initialSeedMenuItem.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pritz_API.Data;
 
@@ -11,9 +12,11 @@ using Pritz_API.Data;
 namespace Pritz_API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230912071650_initialSeedMenuItem")]
+    partial class initialSeedMenuItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
